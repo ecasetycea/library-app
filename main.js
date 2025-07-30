1efc10e0ecfd9cc35504f9e8c16bb0159bd69500
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-07-30 14:22:28 +0300

    Add book constructor

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 window.onload = onLoad;
 
+const library = [];
+
+
+function Book(title, author, pages, read) {
+    if(!new.target) throw Error("Did not call book constructor with 'new' keyword");
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
 
 
 function onLoad() {
@@ -7,5 +20,10 @@ function onLoad() {
 }
 
 function test() {
-    console.log("Hello test");
+    book = new Book("hobbit","tolkien",666,false);
+    book2 = new Book("dune","aragorn",420,true);
+
+    library.push(book, book2);
+
+    console.log(library);
 }
